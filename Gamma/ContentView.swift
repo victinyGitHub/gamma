ba0bc69190e1f86c9b7998075143fe97caa56434
xd123 <xd123@ic.ac.uk>
2024-08-23 18:18:44 +0500

    Initial Commit

---
+++ Gamma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gamma
+//
+//  Created by Xi Da on 23/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gamma/GammaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GammaApp.swift
+//  Gamma
+//
+//  Created by Xi Da on 23/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct GammaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
